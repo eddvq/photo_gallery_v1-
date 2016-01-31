eec7b31b38e8bc547384486b94f4ab964642258b
eddvq <eddyvzqz@gmail.com>
2016-01-30 19:33:11 -0800

    Overlay Shows Image and Information`

--- js/app.js
+++ js/app.js
@@ -25,26 +25,11 @@ $overlay.click(function(){
     $overlay.hide();
 });
 
-
-//Give the input box the value that was passed inside it. 
-
-//Compare that value with the img alt attribute
-
-//If they are not the same hide the image
-
-//when the input bux is out of focus show all images like before
-
-
-//what is e.which???
-//why does it remove all of the images on the first search?
-//Add: If the input box is empty don't run anything //inputText.length === 0
-
-
-
-
-$("#image_name").focus().keyup(function(e){
+$("#image_name").keyup(function(e){
     // This gets the value from the search box
+/*
     if(e.which == 13) {
+*/
         
     var inputTxt = $("#image_name").val();
         
@@ -62,14 +47,26 @@ $("#image_name").focus().keyup(function(e){
         all_Images.each(function(){
         var altTexts = $(this).attr("alt"); 
         
-        if(altTexts.indexOf(inputTxt) === -1){
+        if(altTexts.indexOf(inputTxt) === -1 ){
             $(this).hide();  //turn to hide remove top
+      
            };
-        })
         });
-    };
+        });
+    
+
+    })
+    
+    
+/*
+    };  // this one
+*/
 });
 
+
+
+//foreach instead of each jquery methods for looping 
+//keyup any key 
 
 /*$("#image_name").focus().keyup(function(e){
     if(e.which == 13) {
@@ -90,106 +87,3 @@ $("#image_name").focus().keyup(function(e){
 
     */
         
-/*        var altText = $("img").each(function(){
-            var altTexts = $(this).attr("alt");
-            if(altTexts !== inputTxt){
-                $("img").hide;
-            }
-                });*/
-
-/*        $("img").each(function(){
-            var altTexts = $(this).attr("alt");
-            if(altTexts !== inputTxt){
-                $("img").hide;
-            }
-                });*/
-        
-        
-/*        $("img").each(function(){
-            var altTexts = $("img").attr("alt");
-          console.log(altTexts);
-            console.log(inputTxt);
-            console.log(altTexts);
-
-                });*/
-        
-
-
-/*
-
-so 1. keyup
-2. if 13 {
-3. var inputtxt= form_id.value()
-4. $ ("img").attr("alt").each(){
-if attr("alt").value() !contains inputtxt {
-img.hide()
-}
-}
-
-
-        var altText = $("img").each(function(){
-            var mim = $(this).attr("alt");
-            console.log(mim);
-            
-        });
-       };
-
-$("#image_name").focus().keyup(function(e){
-    if(e.which == 13) {
-       var inputTxt = $("#image_name").val();
-        console.log(inputTxt);
-        var imageAlt =$("img").attr("alt");
-        console.log(imageAlt);
-       };
-});
-
-
-$("#image_name").keyup(function(e){
-    if(e.which == 13) {
-       var inputTxt = $("#image_name").val();
-       var image = $("img").attr("alt");
-        console.log(image);
-       };
-});
-
-
-
-
-
-
-
-so 1. keyup
-2. if 13 {
-3. var inputtxt= form_id.value()
-4. $ ("img").attr("alt").each(){
-if attr("alt").value() !contains inputtxt {
-img.hide()
-}
-}
-
-
-
-
-var $altTag = $("#image_pool").find("alt");
-
-$("#image_name").focus(function(){
-    if($(this).val() != $altTag){
-        console.log('no mimes.');
-    }
-});
-
-
-
-$("div").each(function (i) {
-        if (this.style.color != "blue") {
-          this.style.color = "blue";
-        } else {
-          this.style.color = "";
-        }
-
-
-if it !contains "input str" {
-.hide()
-}
-
-    */
